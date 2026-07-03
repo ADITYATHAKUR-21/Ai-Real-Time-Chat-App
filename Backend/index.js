6aa5a09a267dd1d40c7d686a16cacd8c4e3dda26
Aditya Thakur <adityakr123muz@gmail.com>
2026-07-03 17:51:59 +0530

    Exress setup  is ready

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+
+dotenv.config();
+
+
+
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'OPTION'],
+    allowedHeaders: ['Content-type', "Authorization"]
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+const port = process.env.PORT  || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
